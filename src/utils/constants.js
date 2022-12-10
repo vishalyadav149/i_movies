ad6fb18729d514eb21acafd4b233e2bf50ce310d
vishalyadav149 <vy432505@gmail.com>
2022-12-10 22:55:58 +0530

    First commit

---
+++ src/utils/constants.js
@@ -0,0 +1,42 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.loginradius.com%2Fblog%2Fengineering%2Flazy-loading-in-react%2F&psig=AOvVaw2vdXqiB4Sk6MT_8Lie-L2m&ust=1664560023576000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPDH17fHuvoCFQAAAAAdAAAAABAW';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'Vishal', icon: <CodeIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'ReactJS', icon: <CodeIcon />, },
+  { name: 'NextJS', icon: <CodeIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.loginradius.com%2Fblog%2Fengineering%2Flazy-loading-in-react%2F&psig=AOvVaw2vdXqiB4Sk6MT_8Lie-L2m&ust=1664560023576000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPDH17fHuvoCFQAAAAAdAAAAABAW';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'React';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.loginradius.com%2Fblog%2Fengineering%2Flazy-loading-in-react%2F&psig=AOvVaw2vdXqiB4Sk6MT_8Lie-L2m&ust=1664560023576000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPDH17fHuvoCFQAAAAAdAAAAABAW'
